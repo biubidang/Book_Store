37a24413f0418a0ff90033492cb62c8a1f1d5cb8
biubidang <2814318648@qq.com>
2023-12-23 20:29:27 +0800

    init commit

--- src/main/java/com/sqlwork/book_store/BookStoreApplication.java
+++ src/main/java/com/sqlwork/book_store/BookStoreApplication.java
@@ -1,11 +1,19 @@
 package com.sqlwork.book_store;
 
+import com.baomidou.mybatisplus.autoconfigure.DdlApplicationRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BookStoreApplication {
-
+    @Bean
+    public DdlApplicationRunner ddlApplicationRunner(@Autowired(required = false) List ddlList) {
+        return new DdlApplicationRunner(ddlList);
+    }
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
