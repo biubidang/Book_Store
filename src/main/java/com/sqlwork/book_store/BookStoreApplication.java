6a2feb810e43ee214e384c865f36b391589b74aa
bibiudang <2814318648@qq.com>
2023-12-25 01:26:39 +0800

    解决了mb-plus与springboot3.2.1的兼容问题，项目正常启动

--- src/main/java/com/sqlwork/book_store/BookStoreApplication.java
+++ src/main/java/com/sqlwork/book_store/BookStoreApplication.java
@@ -1,6 +1,7 @@
 package com.sqlwork.book_store;
 
 import com.baomidou.mybatisplus.autoconfigure.DdlApplicationRunner;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
