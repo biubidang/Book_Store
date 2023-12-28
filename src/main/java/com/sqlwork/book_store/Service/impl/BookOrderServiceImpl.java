4f5f12fb9a01407d0ecb641fc1d5f83f5b3e82f0
bibiudang <2814318648@qq.com>
2023-12-28 22:23:50 +0800

    实现采购单生成

--- src/main/java/com/sqlwork/book_store/Service/impl/BookOrderServiceImpl.java
+++ src/main/java/com/sqlwork/book_store/Service/impl/BookOrderServiceImpl.java
@@ -1,11 +1,18 @@
 package com.sqlwork.book_store.Service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.sqlwork.book_store.Mapper.BookOrderMapper;
 import com.sqlwork.book_store.Service.BookOrderService;
+import com.sqlwork.book_store.Service.BookRegistrationsService;
 import com.sqlwork.book_store.domain.ResponseResult;
 import com.sqlwork.book_store.domain.entity.BookOrder;
+import com.sqlwork.book_store.domain.entity.BookRegistrations;
+import com.sqlwork.book_store.enums.HttpCodeEnum;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * (BookOrder)表服务实现类
@@ -15,10 +22,27 @@ import org.springframework.stereotype.Service;
  */
 @Service("bookOrderService")
 public class BookOrderServiceImpl extends ServiceImpl<BookOrderMapper, BookOrder> implements BookOrderService {
+    @Autowired
+    private BookRegistrationsService bookRegistrationsService;
 
     @Override
     public ResponseResult purchase() {
-        
-        return null;
+        BookOrder bookOrder=new BookOrder();
+        float amount=0f;
+        int sum=0;
+        for (BookRegistrations bR: bookRegistrationsService.list()){
+            amount+=bR.getPPrice()*bR.getNumbers();
+            sum+=bR.getNumbers();
+        }
+        if(sum==0){
+            return ResponseResult.errorResult(HttpCodeEnum.SYSTEM_ERROR);
+        }
+        bookOrder.setCategory(0);
+        bookOrder.setCreatetime(new Date());
+        bookOrder.setCustomerId((long) -1);
+        bookOrder.setTotalAmount(amount);
+        bookOrder.setTotalSum(sum);
+        save(bookOrder);
+        return ResponseResult.okResult(bookOrder);
     }
 }
