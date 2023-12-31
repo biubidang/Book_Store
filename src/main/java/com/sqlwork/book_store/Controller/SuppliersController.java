350afc4ea86e4814d644a990f78a0945c3267b6b
biubidang <2814318648@qq.com>
2023-12-31 17:18:17 +0800

    实现供货商查找

--- src/main/java/com/sqlwork/book_store/Controller/SuppliersController.java
+++ src/main/java/com/sqlwork/book_store/Controller/SuppliersController.java
@@ -1,4 +1,20 @@
 package com.sqlwork.book_store.Controller;
 
+import com.sqlwork.book_store.Service.SupplierService;
+import com.sqlwork.book_store.domain.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/supplier")
 public class SuppliersController {
+    @Autowired
+    private SupplierService supplierService;
+
+    @GetMapping("/getSupplier")
+    public ResponseResult getSupplier(String supplier){
+        return supplierService.getSupplier(supplier);
+    }
 }
---
+++ src/main/java/com/sqlwork/book_store/Mapper/SupplierMapper.java
@@ -0,0 +1,15 @@
+package com.sqlwork.book_store.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sqlwork.book_store.domain.entity.Supplier;
+
+
+/**
+ * (Supplier)表数据库访问层
+ *
+ * @author makejava
+ * @since 2023-12-31 16:14:12
+ */
+public interface SupplierMapper extends BaseMapper<Supplier> {
+
+}
